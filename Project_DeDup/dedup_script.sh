c7068e21aafd1d51a33195eb1be8b76b8792d51f
Nadav-Levintov <DSP155@Gmail.com>
2018-03-16 18:37:31 +0200

    script changes with time

--- Project_DeDup/dedup_script.sh
+++ Project_DeDup/dedup_script.sh
@@ -1,45 +1,31 @@
 #!/bin/bash
 CSV=".csv"
 LOG=".log"
-SIZE="512"
+SIZE="4"
 KB="1024"
-TOTALS="524288"
+MB=$((KB*KB))
+TOTALS=$((SIZE*MB))
 DISTANCE="0"
 POINTERS="0"
+rm -rf runs.csv
 
 for FILE in $( ls | grep ".csv" | cut -f 1 -d . ); do
-    mkdir ${FILE}
+    rm -rf ${FILE}
+	mkdir ${FILE}
 
-    ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}KB_D${DISTANCE}_P${POINTERS}${LOG}"
+    /usr/bin/time -f "%E, %M" ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}MB_D${DISTANCE}_P${POINTERS}${LOG}" 
 
-    SIZE="8192"
-    TOTALS=$((SIZE*KB))
-    ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}KB_D${DISTANCE}_P${POINTERS}${LOG}"
-
-
-    SIZE="524288"
-    TOTALS=$((SIZE*KB))
-    ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}KB_D${DISTANCE}_P${POINTERS}${LOG}"
+	POINTERS="128"
+    /usr/bin/time -f "%E, %M" ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}MB_D${DISTANCE}_P${POINTERS}${LOG}" 
 
 
-    SIZE="8192"
-    DISTANCE="2"
+    SIZE="8"
+    TOTALS=$((SIZE*MB))
     POINTERS="0"
-    TOTALS=$((SIZE*KB))
-    ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}KB_D${DISTANCE}_P${POINTERS}${LOG}"
+	/usr/bin/time -f "%E, %M" ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}MB_D${DISTANCE}_P${POINTERS}${LOG}" 
 
-
-    SIZE="524288"
-    DISTANCE="0"
-    POINTERS="4"
-    TOTALS=$((SIZE*KB))
-    ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}KB_D${DISTANCE}_P${POINTERS}${LOG}"
-
-    SIZE="8192"
-    DISTANCE="3"
-    POINTERS="4"
-    TOTALS=$((SIZE*KB))
-    ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}KB_D${DISTANCE}_P${POINTERS}${LOG}"
+    POINTERS="128"
+    /usr/bin/time -f "%E, %M" ./dedup ${FILE}${CSV} ${TOTALS} ${DISTANCE} ${POINTERS} < params > "${FILE}_${SIZE}MB_D${DISTANCE}_P${POINTERS}${LOG}"  
 
     mv *${FILE}*.* ${FILE}
 
